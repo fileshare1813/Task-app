33c42468b2b4093e4eba5519016e627c7c21b7b1
ohi_abhist <fileshare1813@gmail.com>
2026-03-25 16:28:44 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: false },,
+  password: { type: String, required: false },
   googleId: { type: String },
   tasks: [{ type: Schema.Types.ObjectId, ref: 'Task' }]
 }, { timestamps: true });
